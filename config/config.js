a0923fe04b1ef78b2ac691e2223516cbbbf1d9d0
frank-wiseki <tgcode208@gmail.com>
2024-07-07 07:38:21 +0100

    fix connectio  issues

--- config/config.js
+++ config/config.js
@@ -17,11 +17,18 @@ const config ={
     "dialect": "postgres"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 }
 
